8ec6fdaffc351fd12189e3a4b2dc580ec626c322
mcpenguin <marcus.chanwc@gmail.com>
2021-05-30 15:11:49 +0800

    added about me section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
 // import components
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import AboutMe from './components/AboutMe/AboutMe';
 
 export default class App extends Component {
     render() {
         return (
-            <Container fluid>
+            <>
                 <Header />
-            </Container>
+                <AboutMe />
+            </>
         )
     }
 }
