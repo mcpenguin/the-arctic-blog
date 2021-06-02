0b40e293bec3f81247f1f5aadb9a961bf7b5ec2d
mcpenguin <marcus.chanwc@gmail.com>
2021-06-02 23:20:02 +0800

    added pictures for carousel

--- src/components/TopCarousel/TopCarousel.js
+++ src/components/TopCarousel/TopCarousel.js
@@ -13,7 +13,10 @@ import './TopCarousel.css';
 // proportion of carousel images: 1440x600
 
 // import images
-import image1 from './piano-carousel.jpg';
+import notes from './notes_carousel.jpg';
+import piano from './piano_carousel.jpg';
+import github from './github_carousel.jpg';
+import linkedin from './linkedin_carousel.jpg';
 
 export default class TopCarousel extends Component {
     render() {
@@ -22,17 +25,38 @@ export default class TopCarousel extends Component {
                 {/* <Container fluid className="carousel-container"> */}
                     <Carousel>
                         <Carousel.Item>
-                            <img fluid className="carousel-image" src={image1} alt="image1"></img>
+                            <img fluid className="carousel-image" src={notes} alt="image1"></img>
                             <Carousel.Caption>
                                 <h3>First slide label</h3>
                                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img fluid className="carousel-image" src={image1} alt="image1"></img>
+                            <img fluid className="carousel-image" src={notes} alt="image1"></img>
                             <Carousel.Caption>
-                                <h3>Second slide label</h3>
+                                <h3>First slide label</h3>
                                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img fluid className="carousel-image" src={piano} alt="image1"></img>
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img fluid className="carousel-image" src={github} alt="image1"></img>
+                            <Carousel.Caption>
+                                <h1>Want to check out my other projects?</h1>
+                                <h4>Check out my <a className="carousel-link" href="https://github.com/mcpenguin">Github page</a></h4>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img fluid className="carousel-image" src={linkedin} alt="image1"></img>
+                            <Carousel.Caption>
+                                <h1>Want to connect further?</h1>
+                                <h4>Check out my <a className="carousel-link" href="https://www.linkedin.com/in/marcus-chanwc/">LinkedIn page</a></h4>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
