c90342972bda5f40b3b9551bc6bdf724c8dbfa03
mcpenguin <marcus.chanwc@gmail.com>
2021-06-05 09:28:48 +0800

    installed material-ui/icons

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -8,6 +8,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// import material-ui icons
+
+
 // import stylesheet
 import './Timeline.css';
 
