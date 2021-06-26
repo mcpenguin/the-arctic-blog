4d57b04e0f4964e48e84f39811b92c621041c089
mcpenguin <marcus.chanwc@gmail.com>
2021-06-26 20:23:45 +0800

    added projects folder

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,20 @@
+// React component class for Projects section
+import React, { Component } from 'react';
+
+// import Bootstrap components
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// import stylesheet
+import './Projects.css';
+
+export default class Projects extends Component {
+    render() { 
+        return (
+            <>
+            
+            </>
+        ); 
+    }
+}
