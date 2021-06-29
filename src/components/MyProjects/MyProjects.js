43835f55b1ad942b6d85f870260a16bff0a5b07a
mcpenguin <marcus.chanwc@gmail.com>
2021-06-29 20:00:25 +0800

    started implementing projects section page

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -6,15 +6,36 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// import test image
+import test_img from '../../test-1000x1000-2.png'
+
 // import stylesheet
 import './MyProjects.css';
 
 export default class MyProjects extends Component {
     render() { 
         return (
-            <>
-            
-            </>
+            <section class="section-my-projects">
+                <Container fluid>
+                    <Row>
+                        <Col md={6}>
+                            <img className="my-projects-image" src={test_img} />
+                        </Col>
+                        <Col md={6} className="my-projects-text">
+                            <div className="my-projects-text-div">
+                                <h2>My Projects</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing 
+                                    elit, sed do eiusmod tempor incididunt ut labore 
+                                    et dolore magna aliqua. Ut enim ad minim veniam, 
+                                    quis nostrud exercitation ullamco laboris nisi 
+                                    ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         ); 
     }
 }
