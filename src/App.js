f7e0e75a9afb330fade62478ec785adcac8c5da3
mcpenguin <marcus.chanwc@gmail.com>
2021-06-29 18:42:31 +0800

    added imports and react classes of page sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import Fade from 'react-reveal';
 
+// React component for home page
+
 // import components
 import AdBlockerJumbotron from './components/AdBlockerJumbotron/AdBlockerJumbotron';
 import Header from './components/Header/Header';
@@ -9,6 +11,16 @@ import AboutMe from './components/AboutMe/AboutMe';
 import Timeline from './components/Timeline/Timeline';
 import Notes from './components/Notes/Notes';
 import ContactMe from './components/ContactMe/ContactMe';
+
+// import sections to link to pages
+import MyProjects from './components/MyProjects/MyProjects';
+import MyExperience from './components/MyExperience/MyExperience';
+import MyEducation from './components/MyEducation/MyEducation';
+import MyExtracurriculars from './components/MyExtracurriculars/MyExtracurriculars';
+import MyAchievements from './components/MyAchievements/MyAchievements';
+import MyNotes from './components/MyNotes/MyNotes';
+import MyTimeline from './components/MyTimeline/MyTimeline';
+import Credits from './components/Credits/Credits';
 
 
 export default class App extends Component {
@@ -19,8 +31,16 @@ export default class App extends Component {
                 {/* <AdBlockerJumbotron /> */}
                 <TopCarousel/>
                 <AboutMe/>
-                <Timeline />
-                <Notes/>
+                <MyProjects />
+                <MyExperience />
+                <MyEducation />
+                <MyExtracurriculars />
+                <MyAchievements />
+                <MyNotes />
+                <MyTimeline />
+                <Credits />
+                {/* <Timeline /> */}
+                {/* <Notes/> */}
                 <ContactMe/>
             </>
         )
