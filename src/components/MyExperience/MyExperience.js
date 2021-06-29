ec31104b0e4e90cb00d8c36385f86919bbaf2776
mcpenguin <marcus.chanwc@gmail.com>
2021-06-29 21:42:30 +0800

    added experience section, tidied code

--- src/components/MyExperience/MyExperience.js
+++ src/components/MyExperience/MyExperience.js
@@ -6,15 +6,41 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// import test image
+import test_img from '../penguin-test.png'
+
 // import stylesheet
 import './MyExperience.css';
 
 export default class MyExperience extends Component {
     render() { 
         return (
-            <>
-            
-            </>
+            <section class="section-my-experience">
+                <Container fluid>
+                    <Row className="flex-row-reverse">
+                        <Col lg={6} className="col-lg-push-6 my-experience-image-div">
+                            <div>
+                                <img className="my-experience-image" src={test_img} />
+                            </div>
+                        </Col>
+                        <Col lg={6} className="col-lg-pull-6 my-experience-text">
+                            <div className="my-experience-text-div">
+                                <h2>My Experience</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing 
+                                    elit, sed do eiusmod tempor incididunt ut labore 
+                                    et dolore magna aliqua. Ut enim ad minim veniam, 
+                                    quis nostrud exercitation ullamco laboris nisi 
+                                    ut aliquip ex ea commodo consequat.
+                                </p>
+                                <div className="my-experience-view">
+                                    <button class="noselect">View my experience</button>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         ); 
     }
 }
