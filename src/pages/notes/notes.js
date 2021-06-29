991e1b9e23eed1d78ca5b17f06915f66e882ebda
mcpenguin <marcus.chanwc@gmail.com>
2021-06-29 18:32:58 +0800

    added page folders

---
+++ src/pages/notes/notes.js
@@ -0,0 +1,20 @@
+// React component class for notes section
+import React, { Component } from 'react';
+
+// import Bootstrap components
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// import stylesheet
+import './notes.css';
+
+export default class notes extends Component {
+    render() { 
+        return (
+            <>
+            
+            </>
+        ); 
+    }
+}
