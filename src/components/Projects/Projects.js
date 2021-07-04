55c267d9d09808a40f38bf00a71822a8bc82ab47
mcpenguin <marcus.chanwc@gmail.com>
2021-07-04 22:25:45 +0800

    started work on projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,9 +12,18 @@ import './Projects.css';
 export default class Projects extends Component {
     render() { 
         return (
-            <>
-            
-            </>
+            <section class="section-projects-page">
+                <h2>My Projects</h2>
+                <p>
+                    Projects description would go here.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore 
+                    magna aliqua. Ac odio tempor orci dapibus ultrices in 
+                    iaculis nunc sed. Amet risus nullam eget felis eget nunc 
+                    lobortis mattis aliquam. Orci a scelerisque purus semper eget 
+                    duis. Eu augue ut lectus arcu bibendum at varius vel.
+                </p>
+            </section>
         ); 
     }
 }
