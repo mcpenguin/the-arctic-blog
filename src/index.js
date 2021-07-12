9106d08c0100ca83914d0ad52bb732707a6ad4b3
mcpenguin <marcus.chanwc@gmail.com>
2021-07-12 21:13:31 +0800

    uploaded new penguin images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 
 // render app
