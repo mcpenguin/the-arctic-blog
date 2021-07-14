7911e674fa34e555b94a326ab420ba4cea26634e
mcpenguin <marcus.chanwc@gmail.com>
2021-07-14 23:11:24 +0800

    started work on education section

--- src/components/Cover/export-images.js
+++ src/components/Cover/export-images.js
@@ -2,3 +2,4 @@
 
 export {default as projects} from './images/projects.png';
 export {default as experience} from './images/experience.png';
+export {default as education} from './images/education.jpg';
--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -6,15 +6,29 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// import Cover component
+import Cover from '../Cover/Cover';
+
 // import stylesheet
-import './Education.css';
+import './Education.scss';
 
 export default class Education extends Component {
     render() { 
         return (
-            <>
-            
-            </>
+            <section className="section-education-page">
+                <Cover 
+                    coverClass="education"
+                    coverTitle="My Education"
+                    coverDescription={
+                        <p>
+                            I have been working with React for about a year now. I am currently working on
+                            a React based web app that I am building for a client. The app
+                            will be a simple todo list that will allow users to add
+                            tasks and mark them as complete.
+                        </p>
+                    }
+                />
+            </section>
         ); 
     }
 }
