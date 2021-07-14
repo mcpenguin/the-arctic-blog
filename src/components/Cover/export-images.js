d6afb487ccb51abae0742161a31bf53daaecd416
mcpenguin <marcus.chanwc@gmail.com>
2021-07-14 20:32:37 +0800

    started work on experience section

--- src/components/Cover/export-images.js
+++ src/components/Cover/export-images.js
@@ -1,3 +1,4 @@
 // module to export images
 
 export {default as projects} from './images/projects.png';
+export {default as experience} from './images/experience.png';
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,6 +6,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// import Cover
+import Cover from '../Cover/Cover';
+
 // import stylesheet
 import './Experience.css';
 
@@ -13,7 +16,18 @@ export default class Experience extends Component {
     render() { 
         return (
             <>
-            
+                <Cover 
+                    coverClass="experience"
+                    coverTitle="My Experience"
+                    coverDescription={<>
+                        <p>
+                            I have been working with React for about a year now. I am currently working on 
+                            a React based web app that I am building for a client. The app 
+                            will be a simple todo list that will allow users to add 
+                            tasks and mark them as complete.
+                        </p>
+                    </>}
+                />
             </>
         ); 
     }
