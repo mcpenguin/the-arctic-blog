5ba3cbadf4c1e5f5343a9a428cf4f20d9b46981e
mcpenguin <marcus.chanwc@gmail.com>
2021-07-16 23:50:07 +0800

    implemented 404 not found page

--- src/components/404NotFound/404NotFound.js
+++ src/components/404NotFound/404NotFound.js
@@ -7,14 +7,19 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 // import stylesheet
-import './404NotFound.css';
+import './404NotFound.scss';
 
 export default class NotFound extends Component {
-    render() { 
+    render() {
         return (
-            <>
-            
-            </>
-        ); 
+            <section className='section-not-found'>
+                <div className='gradient'>
+                    <div className='desc'>
+                        <h1>404 Error</h1>
+                        <h3>Please check the page you are on is a valid page :D</h3>
+                    </div>
+                </div>
+            </section>
+        );
     }
 }
