479b392dd6db06c02dc0422cab127f5956f45957
mcpenguin <marcus.chanwc@gmail.com>
2021-07-16 23:20:51 +0800

    added content for education

--- src/components/Education/educationData.js
+++ src/components/Education/educationData.js
@@ -4,30 +4,19 @@ export default {
         title: "Data Science, Honours, Co-op @ University of Waterloo",
         location: "Ontario, Canada",
         date: "2020 - 2025",
-        description: <>
-            <p>
-                During the first part of your life, you only become aware of happiness once you have lost it. Then an age comes, a second one, in which you already know, at the moment when you begin to experience true happiness, that you are, at the end of the day, going to lose it. When I met Belle, I understood that I had just entered this second age. I also understood that I hadn’t reached the third age, in which anticipation of the loss of happiness prevents you from living.
-            </p>
-        </>,
         achievements: [
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
+            "Recipient of the CAD$20,000 Faculty of Mathematics Global Scholarship, awarded to only ten international students every year",
+            "One of only three students to transfer from Honours Mathematics to Data Science in first year",
+            "CAV: 96 (MAV: 98)",
         ]
     },
     sunway: {
         title: "Cambridge GCE A-Levels @ Sunway College",
         location: "Kuala Lumpur, Malaysia",
         date: "2018 - 2020",
-        description: <>
-            <p>
-                During the first part of your life, you only become aware of happiness once you have lost it. Then an age comes, a second one, in which you already know, at the moment when you begin to experience true happiness, that you are, at the end of the day, going to lose it. When I met Belle, I understood that I had just entered this second age. I also understood that I hadn’t reached the third age, in which anticipation of the loss of happiness prevents you from living.
-            </p>
-        </>,
         achievements: [
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
-            "Conducted analysis and constructed visualizations for medical data produced by over 40% of the clinics in Malaysia",
+            "Achieved 4 A*s (Chemistry, Physics, Mathematics, Further Mathematics)",
+            "Recipient of the Jeffrey Cheah Entrance Scholarship",
         ]
     },
 };
