a63e4c6fc89321291ee276de6e96feecfd087666
mcpenguin <marcus.chanwc@gmail.com>
2021-07-25 21:29:49 +0800

    fixed image formatting for carousel

--- src/components/TopCarousel/images/export_images.js
+++ src/components/TopCarousel/images/export_images.js
@@ -1,8 +1,8 @@
 // module to export images
 export {default as notes} from './notes_carousel.jpg';
-export {default as piano} from './piano_carousel.jpg';
+// export {default as piano} from './piano_carousel.jpg';
 export {default as graph} from './graph_carousel.png';
-export {default as github} from './github_carousel.jpg';
-export {default as linkedin} from './linkedin_carousel.jpg';
+// export {default as github} from './github_carousel.jpg';
+// export {default as linkedin} from './linkedin_carousel.jpg';
 // export {default as rose} from './rose_carousel.png';
 // export {default as penguin} from './penguin_carousel.png'
