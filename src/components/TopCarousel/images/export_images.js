5134f9390d4db9f05b21d7b39411b547c8ee4ba2
mcpenguin <marcus.chanwc@gmail.com>
2021-07-28 13:24:47 +0800

    added gradient fading to carousel

--- src/components/TopCarousel/images/export_images.js
+++ src/components/TopCarousel/images/export_images.js
@@ -2,6 +2,7 @@
 export {default as notes} from './notes_carousel.jpg';
 // export {default as piano} from './piano_carousel.jpg';
 export {default as graph} from './graph_carousel.png';
+export {default as experience} from './experience_carousel.png';
 // export {default as github} from './github_carousel.jpg';
 // export {default as linkedin} from './linkedin_carousel.jpg';
 // export {default as rose} from './rose_carousel.png';
