977e34dc4404bb494356b1c53c154f65b9e696c0
mcpenguin <marcus.chanwc@gmail.com>
2021-07-28 14:32:35 +0800

    added typewriter effect to about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,18 +8,63 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import personal_photo from './personal-photo.png';
+import Fade from 'react-reveal/Fade';
 
-// import icons from material-ui
-import WorkIcon from '@material-ui/icons/WorkRounded';
-import SchoolIcon from '@material-ui/icons/School';
-import CodeIcon from '@material-ui/icons/CodeSharp';
-import ActivityIcon from '@material-ui/icons/SportsSoccerSharp';
-import NotesIcon from '@material-ui/icons/Create';
+import personal_photo from './personal-photo.png';
 
 import './AboutMe.scss';
 
+const PersonalFact = (props) => {
+    return (
+        <>
+            <h5 className="personal-facts-head">
+            <div className='personal-facts-icon'><i className={props.iconName} /></div>
+            <TypeWriterEffect
+                text={props.title}
+                typeSpeed={100}
+                hideCursorAfterText={true}
+                textStyle={{
+                    fontSize: "1em",
+                }}
+            />
+            </h5>
+            <Fade up delay={2500}>
+                <p className="personal-facts-body">
+                    {props.body}
+                </p>
+            </Fade>
+        </>
+    )
+}
+
 export default class AboutMe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isElementVisible: false,
+        }
+    }
+
+    // get intersection observer to see whether element is in view
+    observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                this.setState({
+                    isElementVisible: entry.isIntersecting,
+                })
+            })
+        },
+        {
+            rootMargin: '0px',
+            // percentage of the windwo you need to see for the animation to start
+            threshold: 0.8,
+        }
+    )
+
+    componentDidMount() {
+        this.observer.observe(document.querySelector('#about-me'));
+    }
+
     render() {
         return (
             <section className="section-about-me" id="about-me">
@@ -41,72 +86,46 @@ export default class AboutMe extends Component {
                                 </div>
                             </Col>
                             <Col lg={6} className="personal-facts-col">
-                                {/* <div className="personal-desc-section">
-                                    <p className="personal-description">
-                                        Welcome to the Arctic Blog!
-                                        My name is Marcus Chan, and I will be entering my 2A term as a Data
-                                        Science student at the University of Waterloo, after my current coop
-                                        term as a data analyst at eMedAsia.
-                                    </p>
-                                    <p className="personal-description">
-                                        I am extremely passionate about mathematics and software
-                                        development, with an aspiration to build a career in data science
-                                        and artificial intelligence after I graduate with a Master's degree or my
-                                        doctorate.
-                                    </p>
-                                </div> */}
                                 <div className='wrapper'>
-                                    <div className="personal-facts">
-                                        <h5 className="personal-facts-head">
-                                            <div className='personal-facts-icon'><i className='fas fa-pencil-alt' /></div>
-                                            {/* <i className='personal-facts-icon fas fa-pencil-alt' /> */}
-                                            Current Notes Update
-                                        </h5>
-                                        <p className="personal-facts-body">
-                                            None (currently in work term)
-                                            {/* <ul>
-                                            <li>MATH 245 (Linear Algebra 2) - Chapter 1</li>
-                                        </ul> */}
-                                        </p>
-
-                                        <h5 className="personal-facts-head">
-                                        <div className='personal-facts-icon'><i className='fas fa-briefcase' /></div>
-                                            Current Coop
-                                        </h5>
-                                        <p className="personal-facts-body">
-                                            Data Analyst, eMedAsia @ Kuala Lumpur, Malaysia
-                                        </p>
-
-                                        <h5 className="personal-facts-head">
-                                        <div className='personal-facts-icon'><i className='fas fa-graduation-cap' /></div>
-                                            Current Education
-                                        </h5>
-                                        <p className="personal-facts-body">
-                                            Data Science, Honours, Co-op (2A in Fall 2021)
-                                            <br />
-                                            University of Waterloo @ Ontario, Canada
-                                            <br />
-                                            CAV: 94 | MAV: 96
-                                        </p>
-
-                                        <h5 className="personal-facts-head">
-                                        <div className='personal-facts-icon'><i className='fas fa-code' /></div>
-                                            Programming Expertise
-                                        </h5>
-                                        <p className="personal-facts-body">
-                                            HTML, CSS (Sass), JS (Node.js, Express.js, React), Python
-                                            (Pandas, Numpy, Matplotlib, BeautifulSoup), Jupyter Notebook, Java (Joget DX), MySQL,
-                                            Racket/Scheme/Lisp, C, Git, Tableau, Metabase
-                                        </p>
-
-                                        <h5 className="personal-facts-head">
-                                        <div className='personal-facts-icon'><i className='far fa-futbol' /></div>
-                                            Hobbies
-                                        </h5>
-                                        <p className="personal-facts-body">
-                                            Mathematics, coding, arranging/composing piano pieces,
-                                            rock climbing, basketball, watching videos on YouTube
-                                        </p>
+                                    <div className={`personal-facts`}>
+                                        <PersonalFact 
+                                            iconName='fas fa-pencil-alt'
+                                            title='Current Notes Update'
+                                            body={<>None (currently in work term)</>}
+                                        />
+                                        <PersonalFact 
+                                            iconName='fas fa-briefcase'
+                                            title='Current Coop'
+                                            body={<>Data Analyst, eMedAsia @ Kuala Lumpur, Malaysia</>}
+                                        />
+                                        <PersonalFact 
+                                            iconName='fas fa-graduation-cap'
+                                            title='Current Education'
+                                            body={<>
+                                                Data Science, Honours, Co-op (2A in Fall 2021)
+                                                <br />
+                                                University of Waterloo @ Ontario, Canada
+                                                <br />
+                                                CAV: 94 | MAV: 96
+                                            </>}
+                                        />
+                                        <PersonalFact 
+                                            iconName='fas fa-code'
+                                            title='Programming Expertise'
+                                            body={<>
+                                                HTML, CSS (Sass), JS (Node.js, Express.js, React), Python
+                                                (Pandas, Numpy, Matplotlib, BeautifulSoup), Jupyter Notebook, Java (Joget DX), MySQL,
+                                                Racket/Scheme/Lisp, C, Git, Tableau, Metabase
+                                            </>}
+                                        />
+                                        <PersonalFact 
+                                            iconName='far fa-futbol'
+                                            title='Hobbies'
+                                            body={<>
+                                                Mathematics, coding, arranging/composing piano pieces,
+                                                rock climbing, basketball, watching videos on YouTube
+                                            </>}
+                                        />
                                     </div>
                                 </div>
                             </Col>
