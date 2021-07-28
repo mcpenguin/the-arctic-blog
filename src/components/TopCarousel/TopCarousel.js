68421a49d46242eccdb51add98d1fa7409b28ce9
mcpenguin <marcus.chanwc@gmail.com>
2021-07-28 14:01:58 +0800

    init individual note page, added animation to topc

--- src/components/TopCarousel/TopCarousel.js
+++ src/components/TopCarousel/TopCarousel.js
@@ -8,6 +8,8 @@ import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
+import Fade from 'react-reveal/Fade';
+
 // import stylesheet
 import './TopCarousel.scss';
 
@@ -49,14 +51,18 @@ export default class TopCarousel extends Component {
                     }
                     <div className='description-sub'>
                         <div className='description'>
-                            <h1>
-                                Welcome to the Arctic Blog :D
-                            </h1>
-                            <h5>
-                                I'm Marcus Chan, a 2A Data Science student
-                                at the University of Waterloo as well as an aspiring
-                                software developer and data scientist.
-                            </h5>
+                            <Fade bottom opposite>
+                                <h1>
+                                    Welcome to the Arctic Blog :D
+                                </h1>
+                            </Fade>
+                            <Fade bottom delay={500} opposite>
+                                <h5>
+                                    I'm Marcus Chan, a 2A Data Science student
+                                    at the University of Waterloo as well as an aspiring
+                                    software developer and data scientist.
+                                </h5>
+                            </Fade>
                         </div>
                     </div>
                 </div>
