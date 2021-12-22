7b638c657e7c857fca7d4ab9d5fcc55b6ea23c48
mcpenguin <marcus.chanwc@gmail.com>
2021-12-22 16:08:45 -0500

    changed gradient for welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,24 +5,20 @@ import './Welcome.scss';
 
 export default function TopCarousel() {
     return (
-        <section className="section-top-carousel">
-            <div className='carousel-container'>
-                <div className='description-sub'>
-                    <div className='description'>
-                        <Fade bottom opposite>
-                            <h1>
-                                Welcome to the Arctic Blog :D
-                            </h1>
-                        </Fade>
-                        <Fade bottom delay={500} opposite>
-                            <h5>
-                                I'm Marcus Chan, a 2A Computer Science student
-                                at the University of Waterloo as well as an aspiring
-                                software developer and data scientist.
-                            </h5>
-                        </Fade>
-                    </div>
-                </div>
+        <section className="section-top-welcome">
+            <div className='description'>
+                <Fade bottom opposite>
+                    <h1>
+                        Welcome to the Arctic Blog :D
+                    </h1>
+                </Fade>
+                <Fade bottom delay={500} opposite>
+                    <h5>
+                        I'm Marcus Chan, a 2A Computer Science student
+                        at the University of Waterloo as well as an aspiring
+                        software developer and data scientist.
+                    </h5>
+                </Fade>
             </div>
         </section>
     );
