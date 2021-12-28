5324f67edc68194c57989a954154673c1d846fd7
mcpenguin <marcus.chanwc@gmail.com>
2021-12-27 22:13:08 -0500

    removed exp and edu, final changes

--- src/components/Education/export_images.js
+++
@@ -1,2 +0,0 @@
-export {default as sunway} from './images/sunway.png';
-export {default as waterloo} from './images/waterloo.png';
--- src/components/Experience/export_images.js
+++
@@ -1,5 +0,0 @@
-// module to export images
-export {default as emedasia} from './images/emedasia.png';
-export {default as dignity} from './images/dignity.png';
-export {default as shelterHome} from './images/shelter-home.png';
-export {default as tms} from './images/tms.jpg';
