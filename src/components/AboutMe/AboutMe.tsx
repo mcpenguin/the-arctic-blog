e55202d2795fbf67b5aaf4a8aa61821cec1328d3
Marcus Chan <marcus.chanwc@gmail.com>
2022-02-28 12:51:05 -0500

    updated personal photo

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -7,7 +7,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import personal_photo from './personal-photo.jpeg';
+import personal_photo from './personal-photo.jpg';
 
 import './AboutMe.scss';
 
